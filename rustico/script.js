5afe29aa851d934e5fb49591721c816390896804
Marcos Alexandria <93044961+marcosalexandria@users.noreply.github.com>
2022-01-21 08:06:42 -0300

    lógica

--- rustico/script.js
+++ rustico/script.js
@@ -3,9 +3,12 @@ logo.style.fontFamily="rustic";
 logo.style.fontSize="2em";
 
 let amostra=document.getElementById("amostra"); //imagem do pedido
-let meuPedido=document.getElementById("meuPedido"); //hambuger
+let meuPedido=document.getElementById("meuPedido"); //hambuguer
+let adicinais=document.getElementsByClassName("incremento"); //adicionais
 
-//mostra a imagem do hamburger, conforme escolha do cliente
+let finalizar=document.getElementById("finalizar"); //botão finalizar
+
+//mostra a imagem do hamburguer, conforme escolha do cliente
 let ver=setInterval(function(){
     if(meuPedido.value=="twistter"){
         amostra.src="imagens/twistter.jpg";
@@ -14,20 +17,35 @@ let ver=setInterval(function(){
     }else if(meuPedido.value=="gourmet"){
         amostra.src="imagens/gourmet.jpg";
     } else{
-        amostra.src="imagens/selecione.jpg"
+        amostra.src="imagens/selecione.jpg";
     }
 },300);
 
 
 
 //Função que mostra os dados do pedido depois de finalizado
-const mostrarPedido=()=>{
+finalizar.addEventListener("click", function(){
+    //hamburguer
     let hambuger="";
+    let incrementos=[];
     if(meuPedido.value=="twistter"){
         hambuger="Twistter";
     }else if(meuPedido.value=="gran-burger"){
         hambuger="Gran-Burger";
     }else if(meuPedido.value=="gourmet"){
         hambuger="Gurmet";
+    };
+    //adicionais
+    if(adicinais[0].checked==true){
+        incrementos.push("Muçarela");
     }
-}
+    if(adicinais[1].checked==true){
+        incrementos.push("Bacon");
+    }
+    if(adicinais[2].checked==true){
+        incrementos.push("Cheddar");
+    }
+    if(adicinais[3].checked==true){
+        incrementos.push("Molho");
+    }
+})
